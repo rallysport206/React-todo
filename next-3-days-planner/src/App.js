6e9ce26630823a7d1a890d17a299f2f5adfc8dd4
rallysport206 <phan.tony125@gmail.com>
2018-08-21 20:50:08 -0700

    added delete entry function

--- next-3-days-planner/src/App.js
+++ next-3-days-planner/src/App.js
@@ -32,4 +32,39 @@ class App extends React.Component {
       })
     }
   }
+  deleteEntry(index, day){
+    if (day ==== 'Today'){
+      let filterList = this.state.Today.filter((elem, i) => {
+        if(i === index){
+          return false;
+        }
+        return true;
+      })
+      this.setState({
+        'Today': filterList
+      });
+    }else if(day === 'Tomorrow'){
+      let filterList = this.state.Tomorrow.filter((elem, i) => {
+        if(i === index){
+          return false;
+        }
+        return true;
+      })
+      this.setState({
+        'Tomorrow': filterList
+      });
+    }else if(day === 'Day_After_Tomorrow'){
+      let filterList = this.state.Day_After_Tomorrow.filter((elem, i) => {
+        if(i === index){
+          return false;
+        }
+        return true;
+      })
+      this.setState({
+        'Day_After_Tomorrow': filterList
+      });
+    }else{
+      console.log ('something went wrong while removing item...');
+    }
+  }
 }
